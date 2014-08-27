af13c811c76f0d6b16c08cc1ae2cd73f1afac8dd
delanni <delanni.alex@gmail.com>
2014-08-27 19:12:27 +0200

    Update code.js

--- code.js
+++ code.js
@@ -3,12 +3,12 @@ var body = document.body;
 var aspect = 228.5/154;
 
 function calculateCanvasDimensions() {
-	if (body.clientHeight-40>body.clientWidth && parseInt(wrapper.style.width)<body.clientWidth){
+	if (body.clientHeight-20>body.clientWidth && parseInt(wrapper.style.width)<body.clientWidth){
 		wrapper.style.width = body.clientWidth + "px";
 		wrapper.style.height = body.clientWidth / aspect + "px";
 	} else {
-		wrapper.style.width = (body.clientHeight-40) * aspect + "px";
-		wrapper.style.height = (body.clientHeight-40) + "px";
+		wrapper.style.width = (body.clientHeight-20) * aspect + "px";
+		wrapper.style.height = (body.clientHeight-20) + "px";
 	}
 	zoom = maxiCanvas.width/miniCanvas.width;
 }
@@ -18,6 +18,8 @@ calculateCanvasDimensions();
 window.onresize = function(event) {
     calculateCanvasDimensions();
 };
+
+setTimeout(calculateCanvasDimensions,500);
 
 document.body.addEventListener('touchmove',function(e){
       e.preventDefault();
