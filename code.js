995ec0e25b30846c9956f21334f40f8553ea1038
delanni <delanni.alex@gmail.com>
2014-08-27 19:23:02 +0200

    Update code.js

--- code.js
+++ code.js
@@ -3,12 +3,12 @@ var body = document.body;
 var aspect = 228.5/154;
 
 function calculateCanvasDimensions() {
-	if (body.clientHeight-20>body.clientWidth && parseInt(wrapper.style.width)<body.clientWidth){
+	if (body.clientHeight*.95>body.clientWidth && parseInt(wrapper.style.width)<body.clientWidth){
 		wrapper.style.width = body.clientWidth + "px";
 		wrapper.style.height = body.clientWidth / aspect + "px";
 	} else {
-		wrapper.style.width = (body.clientHeight-20) * aspect + "px";
-		wrapper.style.height = (body.clientHeight-20) + "px";
+		wrapper.style.width = (body.clientHeight*.95) * aspect + "px";
+		wrapper.style.height = (body.clientHeight*.95) + "px";
 	}
 	zoom = maxiCanvas.width/miniCanvas.width;
 }
