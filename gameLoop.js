2e72cb9ef2c651c99f2c62075dfa9896c2437244
delanni <delanni.alex@gmail.com>
2014-09-10 14:56:06 +0200

    slowmo, crisp

--- gameLoop.js
+++ gameLoop.js
@@ -19,54 +19,47 @@ document.body.addEventListener("keydown", function (e) {
 document.body.addEventListener("keyup", function (e) {
     readInputs.keys[e.keyCode] = false;
 });
+
+miniCanvas.onclick = function(){crisp = !crisp}
+
+var shoot = function(kind){
+	world.addEntity(new kind(parrot.body.center,world), World.COLLIDE_ALL, World.CENTER);
+}
+CMD = [
+	//up
+	Function("targetVector=targetVector==bottom?mid:topV;"),
+	//down
+	Function("targetVector=targetVector==topV?mid:bottom"),
+	// fire
+	Function("shoot(Projectiles.Fireball)"),
+	// water
+	Function("shoot(Projectiles.Waterbolt)"),
+	// poison
+	Function("shoot(Projectiles.Poisonball)"),
+	// lightning
+	Function("shoot(Projectiles.Lightningbolt)"),
+	// slowmo
+	Function("btn","disable(btn);setTimeout(function(){CMD[7](btn)},10e3);timefactor=.25"),
+	// normalmo
+	Function("btn","timefactor=1;setTimeout(function(){enable(btn)},10e3)")
+],
+command = function(id,caller){
+	if (!caller.classList.contains("disabled"))
+	CMD[id](caller);
+}
 Array.prototype.slice.call(document.getElementsByClassName("button")).forEach(function(button){
     button.onmousedown = button.ontouchstart = function(evt){
-        switch(+this.id[0]){
-            case 0:
-                targetVector = topV;
-            break;
-            case 1:
-                targetVector = mid;
-            break;
-            case 2:
-                targetVector = bottom;
-            break;
-            case 3:
-                var proj = new Projectiles.Fireball(parrot.body.center, [0.2,0], 4, world);
-                world.addEntity(proj, World.COLLIDE_ALL, World.CENTER);
-            break;
-            case 4:
-				var proj = new Projectiles.Waterbolt(parrot.body.center, [0.2,0], 3, world);
-                world.addEntity(proj, World.COLLIDE_ALL, World.CENTER);
-            break;
-            case 5:
-				var proj = new Projectiles.Poisonball(parrot.body.center, [0.2,0], 2, world);
-                world.addEntity(proj, World.COLLIDE_ALL, World.CENTER);
-            break;
-            case 6:
-                var proj = new Projectiles.Lightningbolt(parrot.body.center, [0.2,0], 2, world);
-                world.addEntity(proj, World.COLLIDE_ALL, World.CENTER);
-            break;
-        }
+        command(+this.id[0],this);
         evt.preventDefault();
         evt.handled = true;
         return false;
     }
 });
 
-maxiCanvas.addEventListener("mousedown",function(e){
-    var exp = new Effects.Explosion({
-        collisionType:Effects.Explosion.COLLIDE_GROUND
-    });
-
-    exp.fire(new Vector2d(e.offsetX/zoom,e.offsetY/zoom),world);
-});
-
 /// SETUP ENTITIES
 var world = new World();
 
-var heightmap = 15;
-var ground = new GroundEntity(heightmap);
+var ground = new GroundEntity(15,3000);
 world.groundElements.push(ground);
 
 var anim;
@@ -77,7 +70,7 @@ var onLoaded = function(loader){
 	var atlas = loader.spriteSheets["atlas"];
     // SpriteEntity(spritesheet,center,w,h,animations)
     // getAnimation = function(tileWidth,tileHeight, frames, time, start, animwidth?)
-    parrot = new SpriteEntity(atlas,new Vector2d(50,50),16,12,[
+    parrot = new SpriteEntity(atlas,new Vector2d(0,0),16,12,[
         [16,12,6,400,[27,0]]
         ]);
 
@@ -104,7 +97,7 @@ var onLoaded = function(loader){
             s.body.friction = 0;
             world.addEntity(s,World.NO_COLLISION, World.FOREGROUND);
         };
-        tree.body.speed[0]=-.05;
+        tree.body.speed[0]=0;
         tree.body.friction = 0;
         tree.body.center[0]= i * 100;
         world.addEntity(tree, World.COLLIDE_ALL, World.CENTER);
@@ -119,21 +112,29 @@ loader.start(10);
 // SETUP LOOP+FUNCTIONS
 var animate = function(time) {
     // animate animatables
-    parrot.body.gravitateTo(targetVector,time);
+	parrot.body.speed[1] = (targetVector[1]-parrot.body.center[1])*Math.max(time,16)/3000;
+    //parrot.body.gravitateTo(targetVector,time);
+	parrot.body.speed[0]= 0.05;
     world.animate(time);
+	parrot.body.center[1] = clamp(parrot.body.center[1],topV[1],bottom[1]);
 };
 
 var ctx = miniCanvas.getContext("2d");
 ctx.webkitImageSmoothingEnabled = false;
 ctx.imageSmoothingEnabled = false;
-maxiCanvas.getContext("2d").imageSmoothingEnabled = false;
-maxiCanvas.getContext("2d").webkitImageSmoothingEnabled = false;
+translation = 0;
 var render = function(time) {
+	translation = -parrot.body.center[0]+30;
+	ctx.save();
+	ctx.tr(translation,0);
     ctx.fillStyle = P[0];
-    ctx.fillRect(0,0,miniCanvas.width,miniCanvas.height);
+	if (timefactor>0.9)
+	// because the position of this does not move -translation is needed
+    ctx.fr(-translation,0,miniCanvas.width,miniCanvas.height);
 	
 	world.render(ctx,time);
-    maxiCanvas.copyFrom(miniCanvas);
+    //maxiCanvas.copyFrom(miniCanvas);
+	ctx.restore();
 };
 
 var timefactor = 1;
