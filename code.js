e75910647e493c8e8417ea4c55a36efd031fd829
delanni <delanni.alex@gmail.com>
2014-09-12 14:46:53 +0200

    Update code.js

--- code.js
+++ code.js
@@ -5,8 +5,8 @@ var aspect = 228.5/154;
 function calculateCanvasDimensions() {
 	var width = wrapper.offsetWidth;
 	var height = wrapper.offsetHeight;
-	var targetWidth = clamp(body.clientWidth * .95,200,1000);
-	var targetHeight = clamp(body.clientHeight * .95,200/aspect,1000/aspect);
+	var targetWidth = clamp(body.clientWidth * .95,228.5,1000);
+	var targetHeight = clamp(body.clientHeight * .95,228.5/aspect,1000/aspect);
 	var wDiff = width/targetWidth;
 	var hDiff = height / targetHeight;
 	var factor = Math.max(wDiff,hDiff);
@@ -14,7 +14,7 @@ function calculateCanvasDimensions() {
 	wrapper.style.height = width/aspect / factor + "px";
 	veil.style.width = wrapper.style.width;
 	veil.style.height = wrapper.style.height;
-	document.body.style.fontSize = Math.round(16*(wrapper.offsetWidth-200)/800+16) + "px";
+	document.body.style.fontSize = Math.round(16*(wrapper.offsetWidth-228.5)/800+16) + "px";
 }
 
 calculateCanvasDimensions();
